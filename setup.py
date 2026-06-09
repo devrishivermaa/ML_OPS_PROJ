59e37697af0bc0a8e917550a03d785741fa2e660
devrishivermaa <devverma5c@gmail.com>
2026-06-09 16:38:19 +0530

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Dev Verma",
+    author_email="devverma5c@gmail.com",
+    packages=find_packages()
+)
